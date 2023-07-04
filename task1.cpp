3e749987a50a2e3b864d3b7eca5882a856af2027
Barnich-Kate <85077866+Barnich-Kate@users.noreply.github.com>
2023-07-04 13:32:56 +0200

    Create task1.cpp
    Add the solution of the task

---
+++ task1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int k, n;
+    cout<<"Type number n: ";
+    cin >> n;
+    cout<<"Type number k: ";
+    cin >> k;
+    for (int i = 0; i < n; ++i) {
+        cout << k << endl;
+    }
+
+    return 0;
+}
